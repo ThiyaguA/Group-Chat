fad64b44e311f89ed86a5d6ed5a971bf0b166d52
ThiyaguA <thiyaguanto100@gmail.com>
2023-10-25 14:21:55 +0530

    First Commit

---
+++ src/test/java/Chat/GroupChatApplicationTests.java
@@ -0,0 +1,13 @@
+package Chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
